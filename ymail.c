8f78c28990f74f175c61cb1f79c629487716bba7
Athurg Gooth <athurg@gooth.cn>
2010-09-17 12:08:04 +0800

    将数据模型从UI调整到源代码中，增加增量刷新和整体刷新的功能

--- ymail.c
+++ ymail.c
@@ -8,7 +8,10 @@
 #include "mbox.h"
 
 GtkBuilder *builder;
-FILE *mbox_fp;
+
+// 用于存储显示数据的模型
+GtkListStore *list_store;
+
 unsigned int unread=0, total=0;
 
 gboolean status_icon_button_press_cb(GtkStatusIcon *status_icon, GdkEventButton *event, gpointer user_data)
@@ -26,20 +29,45 @@ gboolean status_icon_button_press_cb(GtkStatusIcon *status_icon, GdkEventButton 
 	printf("you press button %d\n", event->button);
 }
 
-void timer_cb(void)
+void release_liststore(void)
 {
-	int status=0;
 	GtkListStore *list_store;
-	char buff[20];
-	static unsigned int lock=0;
-	struct mail_hdr hdr;
-
-
-	sprintf(buff, "未读：%d ；总计：%d ", unread, total);
-	gtk_status_icon_set_tooltip(GTK_STATUS_ICON(gtk_builder_get_object(builder, "status_icon")), buff);
-
 	list_store = GTK_LIST_STORE(gtk_builder_get_object(builder,"liststore"));
-	for(int i=0; i<30; i++){
+
+	printf("Byle\n");
+	g_object_unref(list_store);
+}
+
+int refresh(int type)
+{
+	static FILE *mbox_fp=NULL;
+
+	if(type){//整体刷新
+		if(mbox_fp!=NULL){
+			fclose(mbox_fp);
+			printf("重新打开文件。。。。。");
+		}
+		mbox_fp = fopen("/var/mail/athurg", "rb");
+		if(mbox_fp == NULL){
+			perror("打开邮箱失败");
+			return -1;
+		}
+		printf("整体刷新。。。。。");
+		gtk_list_store_clear(list_store);
+	}else{
+		if(mbox_fp==NULL){
+			mbox_fp = fopen("/var/mail/athurg", "rb");
+			if(mbox_fp == NULL){
+				perror("打开邮箱失败");
+				return -1;
+			}
+			printf("打开文件。。。。。");
+		}
+		printf("增量刷新。。。。。");
+	}
+
+	for(int i=0; i<1000; i++){
+		struct mail_hdr hdr;
 		GtkTreeIter iter;
 		int ret=0;
 		char strstat[10]={0};
@@ -76,34 +104,60 @@ void timer_cb(void)
 				-1);
 		free_mail_hdr(&hdr);
 	}
-
+	printf("完成\n");
 }
 
-int init_gui(int argc, char *argv[])
+
+int refresh_all(void)
 {
+	refresh(1);
+}
+int prog_init(int argc, char *argv[])
+{
+	GtkTreeView *tree_view;
+
+
 	gtk_init(&argc, &argv);
+
+	//数据模型初始化
+	//model = GTK_TREE_MODEL(gtk_list_store_new(5,
+	list_store = gtk_list_store_new(5,
+				G_TYPE_STRING,
+				G_TYPE_STRING,
+				G_TYPE_STRING,
+				G_TYPE_STRING,
+				G_TYPE_STRING);
+
+	//图形界面初始化
 
 	builder = gtk_builder_new();
 	gtk_builder_add_from_file(builder, "ui.glade", NULL);
 	gtk_builder_connect_signals(builder, NULL);
 
+	tree_view= GTK_TREE_VIEW(gtk_builder_get_object(builder,"treeview"));
+	//gtk_tree_view_set_model(tree_view, model);
+	gtk_tree_view_set_model(tree_view, GTK_TREE_MODEL(list_store));
+
+	//释放Builder占用的内存（多达50M啊！！！）
+	g_object_unref(builder);
+
+	//定时每3秒刷新检测一次邮件
+	gtk_timeout_add(1000*3, (GtkFunction) refresh, NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	init_gui(argc, argv);
 	struct mail_hdr hdr={NULL};
 
-	// 检查邮箱是否有效
-	mbox_fp = fopen("/var/mail/athurg", "rb");
-	if(mbox_fp == NULL){
-		perror("打开邮箱失败");
-		return -1;
-	}
 
-	gtk_timeout_add(1000, (GtkFunction) timer_cb, NULL);
+	prog_init(argc, argv);
+
+
 	gtk_main();
-	fclose(mbox_fp);
+
+	//退出时释放内存
+	//g_object_unref(model);
+
 	return 0;
 }
 
